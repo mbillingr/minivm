980ec55aee2c4f7d9524fe6929bb3033ffb1c26f
mbilling <flkazemakase@gmail.com>
2019-08-08 14:37:33 +0200

    don't compile experimental "languages"

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ pub mod ssa_builder;
 
 pub mod assembler;
 pub mod bytecode_builder;
-pub mod closure_lang;
+//pub mod closure_lang;
 pub mod core_compiler;
 pub mod core_cps_compiler;
 pub mod direct_scheme;
-pub mod fixlang;
+//pub mod fixlang;
 pub mod memory;
-pub mod nolambda;
+//pub mod nolambda;
 pub mod primitive_value;
 pub mod scheme_parser;
 pub mod test_utils;
